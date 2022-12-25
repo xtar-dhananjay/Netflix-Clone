7a8c34bf92a4a299f6641ccf86e9ae83de50bc73
xtar-dhananjay <xtardhananjay@gmail.com>
2022-12-25 22:05:28 +0530

    In this commit this is almost complete. in netflix clone version footer was not add.

---
+++ Js/Script.js
@@ -0,0 +1,28 @@
+// Mkae accordion
+let allAccordion = document.querySelectorAll('#accordionCont .accordionHeading');
+allAccordion.forEach( (e, ei) => {
+    e.onclick = () => {
+        let collapseIcon = e.closest('.accordion').querySelector('i');
+        let accordionContent = e.closest('.accordion').querySelector('.accordionContent');
+
+        if (collapseIcon.classList.contains('fa-plus')) {
+            collapseIcon.classList.replace('fa-plus','fa-xmark');
+        }else{
+            collapseIcon.classList.replace('fa-xmark','fa-plus');
+        }
+        accordionContent.classList.toggle('accordionColaps');
+
+        // Collapse all accordions leaving the current accordion
+        allAccordion.forEach((x, xi) => {    
+            if (xi != ei) {
+                
+                let xaccordionContent = x.closest('.accordion').querySelector('.accordionContent');
+                xaccordionContent.classList.remove('accordionColaps');
+                let xcollapseIcon = x.closest('.accordion').querySelector('i');
+                xcollapseIcon.classList.replace('fa-xmark','fa-plus');
+
+            }
+
+        });
+    }
+})
